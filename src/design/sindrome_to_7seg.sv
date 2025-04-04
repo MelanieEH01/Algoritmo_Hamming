ac4c010dd728d61484ec6dd1a2f646d6be39ef86
Melanie Espinoza <melanie.espinoza.hdez@gmail.com>
2025-04-04 01:42:20 -0600

    Final Update

--- src/design/sindrome_to_7seg.sv
+++ src/design/sindrome_to_7seg.sv
@@ -4,17 +4,14 @@ module sindrome_to_7seg(
 );
     
     // Mapeo de síndrome a 7 segmentos (común cát - 0 activa el segmento)
-    always_comb begin
-        case (sin)
-            3'b000: seg = 7'b1000000; // 0
-            3'b001: seg = 7'b1111001; // 1
-            3'b010: seg = 7'b0100100; // 2
-            3'b011: seg = 7'b0110000; // 3
-            3'b100: seg = 7'b0011001; // 4
-            3'b101: seg = 7'b0010010; // 5
-            3'b110: seg = 7'b0000010; // 6
-            3'b111: seg = 7'b1111000; // 7
-            default: seg = 7'b1111111; // s apagados
-        endcase
-    end
+    assign seg = (sin == 3'b000) ? 7'b1000000 : // 0
+                 (sin == 3'b001) ? 7'b1111001 : // 1
+                 (sin == 3'b010) ? 7'b0100100 : // 2
+                 (sin == 3'b011) ? 7'b0110000 : // 3
+                 (sin == 3'b100) ? 7'b0011001 : // 4
+                 (sin == 3'b101) ? 7'b0010010 : // 5
+                 (sin == 3'b110) ? 7'b0000010 : // 6
+                 (sin == 3'b111) ? 7'b1111000 : // 7
+                7'b1111111;  // default: segmentos apagados
+
 endmodule
